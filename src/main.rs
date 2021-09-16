d9ae2f9c4bb97242f2309a81a21e34277387db06
PrisionMike <su.sh2396@gmail.com>
2021-09-17 00:29:53 +0530

    on to formal edits

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,16 @@ fn main() {
 
     let matches = App::new("JHAADI")
         .version("0.1.0")
-        .author("Suyash Shandilya <su.sh2396@gmail.com>")
+        .author("Prison Mike <su.sh2396@gmail.com>")
         .about("API Response Buffer")
-        .arg(Arg::with_name("naam")
-                .short("k")
-                .long("kbaathogayi")
+        .arg(Arg::with_name("theapi")
+                .short("i")
+                .long("api")
                 .takes_value(true)
                 .help("Just say 'Peechhe' and get it over with"))
         .get_matches();
 
-        let position = matches.value_of("naam").unwrap_or("neeche moot de");
+        let position = matches.value_of("naam").unwrap();
         println!("input received: {}", position);
 
 
