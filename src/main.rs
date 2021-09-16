f8cb5e3ed8fb91c76fdce05143d00e98d6a669a1
PrisionMike <su.sh2396@gmail.com>
2021-09-16 23:52:39 +0530

    CL parse function added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Shree Ram Jaanki...");
+
+    let matches = App::new("JHAADI")
+        .version("0.1.0")
+        .author("Suyash Shandilya <su.sh2396@gmail.com>")
+        .about("API Response Buffer")
+        .arg(Arg::with_name("naam")
+                .short("k")
+                .long("kbaathogayi")
+                .takes_value(true)
+                .help("Just say 'Peechhe' and get it over with"))
+        .get_matches();
+
+        let position = matches.value_of("naam").unwrap_or("neeche moot de");
+        println!("input received: {}", position);
+
+
 }
