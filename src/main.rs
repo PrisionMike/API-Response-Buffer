8b4c0c3ed9c126023a8d140abf3622ed68496e4d
PrisionMike <su.sh2396@gmail.com>
2021-11-06 11:59:26 +0530

    onto parsing json

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::{Arg, App};
 use reqwest;
 use std::collections::VecDeque;
+use serde::{Deserialize, Serialize};
+use serde_json;
 #[macro_use] extern crate rocket;
 
 
@@ -75,25 +77,33 @@ async fn main() -> () {
                 .long("size")
                 .takes_value(true)
                 .help("Number of Responses to cache"))
+        .arg(Arg::with_name("JSON_data_section")
+                .short("s")
+                .long("section")
+                .takes_value(true)
+                .help("Enter the data field for the expected JSON repsonse."))
         .get_matches();
 
         let the_api = matches.value_of("theapi").unwrap_or("Jhingalala");
         let cap = matches.value_of("capacity").unwrap_or("hu hu");
-        println!("input received: {}\n{}", the_api, cap);
+        let dataf = matches.value_of("JSON_data_section");
+        println!("input received:\n
+                  API:  {}\n
+                  Capacity: {}\n",&the_api,&cap);
+        if let Some(v) = dataf {
+                println!("Data field: {}",v);
+        }
 
-        // cap = ();
-        let mut capint : usize = usize::from_str_radix(cap, 10).unwrap();
-        capint += 1;
-        // println!("{}",capint);
-        let mut tank: VecDeque<String> = VecDeque::with_capacity(capint);
-        for i in 1 .. capint {
+        let capint : usize = usize::from_str_radix(cap, 10).unwrap() + 1;
+        
+        let mut text_tank: VecDeque<String> = VecDeque::with_capacity(capint);
+        for _i in 1 .. capint {
                 let res = updog(the_api).await;
-                tank.push_back(res.unwrap());
-                // println!("{}",i)
+                text_tank.push_back(res.unwrap());
         }
 
         for _i in 1 .. capint {
-                let popped =  tank.pop_front();
+                let popped =  text_tank.pop_front();
                 if let Some(v) = popped {
                         println!("{}",v);
                 }
