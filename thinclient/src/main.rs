54b4396a61f02ebc4df8dc027f6df6124362bd9d
PrisionMike <su.sh2396@gmail.com>
2021-11-26 21:10:36 +0530

    thinclient for testing

--- thinclient/src/main.rs
+++ thinclient/src/main.rs
@@ -1,3 +1,9 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    let socket = "localhost:23541";
+    let mut stream = TcpStream::connect(socket).unwrap();
+    // let req = "GIVE 3";
+    stream.write(b"GIVE 69");
 }
---
+++ thinclient/target/debug/thinclient.d
@@ -0,0 +1,1 @@
+I:\Rustspace\api_resp_buff\thinclient\target\debug\thinclient.exe: I:\Rustspace\api_resp_buff\thinclient\src\main.rs
